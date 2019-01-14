c929fe096c6f2219452448934220b04fdf5a620c
Enrique Hermida <enriqueh.cehd@outlook.com>
2019-01-14 12:26:03 -0600

    MasterPage
    Base Del master page xd

---
+++ RQTools/RQTools/Views/MasterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace RQTools.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPage : MasterDetailPage
+	{
+		public MasterPage ()
+		{
+			
+		}
+	}
+}
