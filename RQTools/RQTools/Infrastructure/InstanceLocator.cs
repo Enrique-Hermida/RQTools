6c0957489fdeebe34795932d698f317286bdec60
Enrique Hermida <enriqueh.cehd@outlook.com>
2019-01-14 12:02:17 -0600

    Login Page
    Base de la LoginPage

---
+++ RQTools/RQTools/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RQTools.Infrastructure
+{
+    using ViewModels;
+    class InstanceLocator
+    {
+        #region Propreties
+        public MainViewModel Main { get; set; }
+        #endregion
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ RQTools/RQTools/ViewModels/LoginViewModel.cs
@@ -0,0 +1,81 @@
+﻿namespace RQTools.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    class LoginViewModel : BaseViewModel
+    {
+        #region Atributos
+        private string password;
+        private string email;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+        #region Properties
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+        #region Constructores
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.isEnabled = true;
+        }
+        #endregion
+        #region Comandos
+        public ICommand LoginCommand {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debes ingresar tu Email",
+                    "Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debes ingresar tu Contraseña",
+                    "Aceptar");
+                return;
+            }
+        }
+        #endregion
+    }
+}
