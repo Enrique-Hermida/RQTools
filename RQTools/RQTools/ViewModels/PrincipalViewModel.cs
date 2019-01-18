9538ff65d111e3dc56319a371182cc726bba5c5b
Enrique Hermida <enriqueh.cehd@gmail.com>
2019-01-18 17:08:11 -0600

    PrincipalPage
    modificaciones para ver si se ligo la pagina

--- RQTools/RQTools/ViewModels/PrincipalViewModel.cs
+++ RQTools/RQTools/ViewModels/PrincipalViewModel.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RQTools.ViewModels
+﻿namespace RQTools.ViewModels
 {
-    class PrincipalViewModel
+    using GalaSoft.MvvmLight.Command;
+    using RQTools.Views;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+    class PrincipalViewModel:BaseViewModel
     {
+        #region Comandos
+        public ICommand Shospiltalcommand
+        {
+            get
+            {
+                return new RelayCommand(Shospital);
+            }
+        }
 
+        private async void Shospital()
+        {
+            await Application.Current.MainPage.DisplayAlert(
+                    "Bien",
+                    "Si funciona este desvergue xd",
+                    "Aceptar");
+            return;
+        }
+
+        #endregion
     }
 }
