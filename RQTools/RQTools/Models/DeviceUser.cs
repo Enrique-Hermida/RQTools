2211989e5bb363b8fd8c1caac54983d490d2072b
Enrique Hermida <enriqueh.cehd@outlook.com>
2019-02-22 21:16:42 -0600

    Models DeviceUsers
    se agregaron las librerias correspondientes para que funcione con la bd y con json

---
+++ RQTools/RQTools/Models/DeviceUser.cs
@@ -0,0 +1,18 @@
+﻿namespace RQTools.Models
+{
+    using SQLite.Net.Attributes;
+    public class DeviceUser
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID_User { get; set; }
+        public string Name_User { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public int User_Type { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", ID_User, Name_User, Correo, Password);
+        }
+    }
+}
