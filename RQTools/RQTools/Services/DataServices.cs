dd9ada7d35f556fb64d8f04f66032ab4e4f2ba20
Enrique Hermida <enriqueh.cehd@outlook.com>
2019-03-08 21:39:12 -0600

    Table Hospital

--- RQTools/RQTools/Services/DataServices.cs
+++ RQTools/RQTools/Services/DataServices.cs
@@ -24,7 +24,10 @@
             var databasePath = DependencyService.Get<IPathService>().GetDatabasePath();
             this.connection = new SQLiteAsyncConnection(databasePath);
             await connection.CreateTableAsync<DeviceUser>().ConfigureAwait(false);
+            await connection.CreateTableAsync<HospitalModel>().ConfigureAwait(false);
         }
+
+        #region Modelo Usuario
         public async Task<List<DeviceUser>> GetAllUsers()
         {
             var query = await this.connection.QueryAsync<DeviceUser>("select * from [DeviceUser]");
@@ -38,8 +41,32 @@
                 User_Type = u.User_Type
             }).ToList();
             return list;
-
         }
+        public async Task DelleteAllUsers()
+        {
+            var query = await this.connection.QueryAsync<DeviceUser>("delete from [DeviceUser]");
+        }
+        #endregion
+
+        #region Modelo Hospitales
+        public async Task<List<HospitalModel>> GetAllHospitals()
+        {
+            var query = await this.connection.QueryAsync<HospitalModel>("select * from [HospitalModel]");
+            var array = query.ToArray();
+            var list = array.Select(h => new HospitalModel
+            {
+                ID_Hospital = h.ID_Hospital,
+                Nombre_Hospital=h.Nombre_Hospital,
+                Codigo_Hospital=h.Codigo_Hospital,
+                Activo=h.Activo
+            }).ToList();
+            return list;
+        }
+        public async Task DelleteAllHospitals()
+        {
+            var query = await this.connection.QueryAsync<HospitalModel>("delete from [HospitalModel]");
+        }
+        #endregion
         public async Task Insert<T>(T model)
         {
             await this.connection.InsertAsync(model);
@@ -56,9 +83,6 @@
         {
             await this.connection.DeleteAsync(model);
         }
-        public async Task DelleteAllUsers()
-        {
-            var query = await this.connection.QueryAsync<DeviceUser>("delete from [DeviceUser]");
-        }
+        
     }
 }
